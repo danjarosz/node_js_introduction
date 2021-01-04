82588f0ded95418f5c695d1a246c00c022f6ba1d
danjarosz <jarosz.daniel@protonmail.com>
2021-01-04 18:03:56 +0100

    feat(path): parse, extname, isAbsolute

--- module-path/index.js
+++ module-path/index.js
@@ -1,7 +1,29 @@
 const path = require("path");
 
+//JOIN
 const pathToFile = path.join(__dirname, "index.js");
 console.log(pathToFile);
 
 const anotherPathToFile = path.join(__dirname, "..", "users", "index.js");
 console.log(anotherPathToFile);
+
+//PARSE
+const parse = path.parse(__filename);
+console.log(parse);
+
+const parseTwo = path.parse(path.join(__dirname, "index.js"));
+console.log(parseTwo);
+
+//EXTNAME
+const extname = path.extname("index.js");
+console.log(extname);
+
+//ISABSOLUTE
+const isAbsolute = path.isAbsolute("./file.js");
+console.log(isAbsolute);
+
+const isAbsoluteTwo = path.isAbsolute("/file.js");
+console.log(isAbsoluteTwo);
+
+const isAbsoluteThree = path.isAbsolute("file.js");
+console.log(isAbsoluteThree);
