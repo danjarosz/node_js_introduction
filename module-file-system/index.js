bf4109c09e1bc45bedbc1eab4a86195b5031f993
danjarosz <jarosz.daniel@protonmail.com>
2021-01-04 17:04:35 +0100

    feat(fs): access, rename

---
+++ module-file-system/index.js
@@ -0,0 +1,52 @@
+const fs = require("fs");
+
+//ACCESS
+fs.access("./names.txt", (err) => {
+  const message = err
+    ? "Plik names.txt nie istnieje"
+    : "Plik names.txt istnieje";
+  console.log(message);
+});
+
+fs.access("./names.txt", fs.constants.F_OK, (err) => {
+  const message = err
+    ? "Plik names.txt nie istnieje"
+    : "Plik names.txt istnieje";
+  console.log(message);
+});
+
+fs.access("./zablokowany.txt", fs.constants.W_OK, (err) => {
+  const message = err
+    ? "Plik zablokowany.txt nie można zapisywać"
+    : "Plik zablokowany.txt można zapisywać";
+  console.log(message);
+});
+
+//RENAME
+fs.rename("./names.txt", "./imiona.txt", (err) => {
+  if (err) {
+    console.log(err);
+  }
+
+  console.log("Nazwa zmieniona");
+});
+
+fs.rename("./imiona.txt", "./names.txt", (err) => {
+  if (err) {
+    console.log(err);
+  }
+
+  console.log("Nazwa zmieniona");
+});
+
+try {
+  fs.renameSync("./users.txt", "./uzytkownicy.txt");
+} catch (err) {
+  console.log(err);
+}
+
+try {
+  fs.renameSync("./uzytkownicy.txt", "./users.txt");
+} catch (err) {
+  console.log(err);
+}
