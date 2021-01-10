1633d6d38f21de9ff5a0f7758fda0f1e131bb298
danjarosz <jarosz.daniel@protonmail.com>
2021-01-10 20:41:10 +0100

    refactor: extract game.js

---
+++ express_example_project/routes/game.js
@@ -0,0 +1,46 @@
+function gameRoute(app) {
+  let goodAnswers = 0;
+  let callToAFriendUsed = false;
+  let questionToTheCrowdUsed = false;
+  let halfOnHalfUsed = false;
+
+  const questions = [
+    {
+      question: "Jaki jest najlepszy język programownaia wg mnie?",
+      answers: ["C++", "Fortran", "JavaScript", "Java"],
+      correctAnswer: 2,
+    },
+    {
+      question: "Czy ten kurs jest fajny",
+      answers: ["Nie wiem", "Oczywiście, że tak", "Nie", "Jest najlepszy"],
+      correctAnswer: 3,
+    },
+    {
+      question: "Czy chcesz zjeść pizzę",
+      answers: [
+        "Nawet dwie",
+        "Jestem na diecie",
+        "Nie, dziękuję",
+        "Wolę brokuły",
+      ],
+      correctAnswer: 0,
+    },
+  ];
+
+  app.get("/question", (req, res) => {
+    if (goodAnswers === questions.length) {
+      res.json({
+        winner: true,
+      });
+    } else {
+      const nextQuestion = questions[goodAnswers];
+      const { question, answers } = nextQuestion;
+      res.json({
+        question,
+        answers,
+      });
+    }
+  });
+}
+
+module.exports = gameRoute;
