ef49bbcdeef153f33d95367f9146fb45d42054e6
danjarosz <jarosz.daniel@protonmail.com>
2021-01-13 19:45:39 +0100

    feat(mongodb): CRUD in Node

--- mongodb/mongo_in_node/index.js
+++ mongodb/mongo_in_node/index.js
@@ -16,13 +16,66 @@ client.connect((err) => {
     const db = client.db("test");
 
     //Wybór odpowiedniej kolekcji
-    const clients = db.collection("clients");
+    const cars = db.collection("cars");
 
     //operacje na bazie
-    //...
-    //koniec operacji na bazie
+    //INSERT
+    // cars.insertOne(
+    //   {
+    //     brand: "Honda",
+    //     model: "Civic",
+    //   },
+    //   (err) => {
+    //     if (err) {
+    //       console.log("Błąd podczas dodawnia danych");
+    //     } else {
+    //       console.log("Pomyślnie dodano do bazy danych");
+    //     }
+    //     //Zakończenie połączenia z bazą danych
+    //     // client.close();
+    //     client.close();
+    //   }
+    // );
 
-    //Zakończenie połączenia z bazą danych
-    client.close();
+    //DELETE
+    // cars.deleteOne(
+    //   {
+    //     _id: mongo.ObjectId("5fff3e48d41e7656fd8e0277"),
+    //   },
+    //   (err) => {
+    //     if (err) {
+    //       console.log("Błąd podczas usuwania danych");
+    //     } else {
+    //       console.log("Pomyślnie usunięto z bazy danych");
+    //     }
+    //     //Zakończenie połączenia z bazą danych
+    //     // client.close();
+    //     client.close();
+    //   }
+    // );
+
+    //FIND
+    cars.find({}).toArray((err, carsList) => {
+      if (err) {
+        console.log("Błędne zapytanie");
+      } else {
+        console.log(carsList);
+      }
+      client.close();
+    });
+
+    //UPDATE
+    // cars.updateOne(
+    //   { _id: mongo.ObjectId("5ffdeaf68a2c239c15adbac6") },
+    //   { $set: { model: "Lanos" } },
+    //   (err) => {
+    //     if (err) {
+    //       console.log("Błąd podczas modyfikowania danych");
+    //     } else {
+    //       console.log("Pomyślnie zmodyfikowano dane");
+    //     }
+    //     client.close();
+    //   }
+    // );
   }
 });
